8aef77d9dc02f16bc26e67a0cdbb6169eebb6948
RichardEsquivel <rlesquivel@gmail.com>
2019-11-04 11:28:01 -0600

    Staring project

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,7 +6,13 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+	db.select('*').from('posts') //all knex commands return a promise
+		.then(posts => {
+			res.status(200).json(posts);
+		})
+		.catch(error => {
+			res.status(500).json(error);
+		})
 });
 
 router.get('/:id', (req, res) => {
@@ -14,7 +20,10 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-
+	const postData = req.body;
+	//you want to always validate the data coming from the client haven't done that here but I should have
+	///always with insert have an id for the 2nd argument 
+	db('posts').insert(postData, 'id');
 });
 
 router.put('/:id', (req, res) => {
